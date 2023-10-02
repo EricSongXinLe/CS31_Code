1539a429b3c59612a5a1b04367e64f7c78df2a79
EricS <song_xinle@163.com>
2023-10-02 14:05:40 -0700

    helloWorld test
    git test

---
+++ helloWorld/helloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Eric on 2023/10/2.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n" << endl;
+    return 0;
+}
