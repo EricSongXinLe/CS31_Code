64b2777f12e33d694a3af5e3e8eb8d374e0d30b2
EricS <song_xinle@163.com>
2023-10-03 16:13:30 -0700

    task 6&7 complete
    report.docx pending

---
+++ Project1/compile_error/compile_error/compile_error/compile_error.cpp
@@ -0,0 +1,34 @@
+// Code for Project 1
+    // Report rice survey results
+    
+    #include <iostream>
+    using namespace std;
+    
+    int main()
+    {
+        //compile error 1: MISSING int numberSurveyed;
+        int preferBasmati;
+        int preferJasmine;
+    
+        cout << "How many people were surveyed? ";
+        cin >> numberSurveyed; //error 1 cause:'Use of undeclared identifier'
+        cout << "How many of them prefer basmati rice over jasmine rice? ";
+        cin >> preferBasmati;
+        cout << "How many of them prefer jasmine rice over basmati rice? ";
+        cin >> preferJasmine //compile error 2: MISSING ';' after expression
+    
+        double pctBasmati = 100.0 * preferBasmati / numberSurveyed;
+        double pctJasmine = 100.0 * preferJasmine / numberSurveyed;
+
+        cout.setf(ios::fixed);
+        cout.precision(1);
+    
+        cout << endl;
+        cout << pctBasmati << "% prefer basmati rice over jasmine rice." << endl;
+        cout << pctJasmine << "% prefer jasmine rice over basmati rice." << endl;
+
+        if (preferBasmati > preferJasmine)
+            cout << "More people prefer basmati rice over jasmine rice." << endl;
+        else
+            cout << "More people prefer jasmine rice over basmati rice." << endl;
+    }
