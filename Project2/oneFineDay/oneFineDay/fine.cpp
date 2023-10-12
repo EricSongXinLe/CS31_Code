f4ce2370a08740b496082fd7f83c478a02e97498
EricS <song_xinle@163.com>
2023-10-12 15:34:02 -0700

    fine.cpp complete
    check pending

--- Project2/oneFineDay/oneFineDay/fine.cpp
+++ Project2/oneFineDay/oneFineDay/fine.cpp
@@ -1,57 +1,53 @@
-//
-//  main.cpp
-//  oneFineDay
-//
-//  Created by Eric on 2023/10/11.
-//
-
 #include <iostream>
 using namespace std;
 
 int main() {
     string propIdentification;
-    bool passedVerification;
-    long overstateAmount;
     string location;
+    double overstateAmount = 0;
+    double fine = 0;
+    double addRate = 0.153;
     
-    passedVerification = false;
-    while( not passedVerification){
-        cout << "Identification: ";
-        getline(cin,propIdentification);
-        if (propIdentification == ""){
-            cout << "You must enter a property identification." << endl;
-            passedVerification = false;
-        }
-        else{
-            passedVerification = true;
-        }
+    cout << "Identification: ";
+    getline(cin,propIdentification);
+    if (propIdentification == ""){
+        cout << "You must enter a property identification." << endl;
+        return 1;
     }
     
-    passedVerification = false;
-    while (not passedVerification){
-        cout <<"Overstated amount (in millions): ";
-        cin >> overstateAmount;
-        if (overstateAmount<=0){
-            cout<<"The overstated amount must be positive."<<endl;
-            passedVerification = false;
-        }
-        else{
-            passedVerification = true;
-        }
+    
+    cout <<"Overstated amount (in millions): ";
+    cin >> overstateAmount;
+    cin.ignore(10000, '\n');
+    if (overstateAmount<=0){
+        cout<<"The overstated amount must be positive."<<endl;
+        return 1;
+    }
+
+    
+    cout <<"Location: ";
+    getline(cin,location);
+    if (location == ""){
+        cout << "You must enter a location." << endl;
+        return 1;
     }
     
-    passedVerification = false;
-    while (not(passedVerification)){
-        cout <<"Location: ";
-        getline(cin,location);
-        if (location == ""){
-            cout << "You must enter a location." << endl;
-            passedVerification = false;
-        }
-        else{
-            passedVerification = true;
-        }
+    if (location == "florida" || location == "new york"){
+        addRate = 0.18;
     }
     
+    if (overstateAmount <= 60){
+        fine = overstateAmount*0.1111;
+    }
+    else if (overstateAmount <=150){
+        fine = 60*0.1111+(overstateAmount-60)*addRate;
+    }
+    else{
+        fine = 60*0.1111+90*addRate+(overstateAmount-150)*0.21;
+    }
+    cout<<"---"<<endl;
+    cout.setf(ios::fixed);
+    cout.precision(3);
+    cout<<"The fine for "<<propIdentification<<" is $"<<fine<<" million."<<endl;
     return 0;
 }
