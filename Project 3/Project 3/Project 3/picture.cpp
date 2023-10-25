ac7c4ee173c3e5d4fe9b6e03002b648cc671c6b5
EricS <song_xinle@163.com>
2023-10-25 14:08:27 -0700

    comments added

--- Project 3/Project 3/Project 3/picture.cpp
+++ Project 3/Project 3/Project 3/picture.cpp
@@ -1,16 +1,16 @@
 #include "grid.h"
 
 void plotHorizontalLine(int r, int c, int distance, char ch){
-    const int GRAPH_ROWS = getRows();
+    const int GRAPH_ROWS = getRows(); //check the maximum column/row in the graph.
     const int GRAPH_COLS = getCols();
     int currentCol;
-    for (int i =0; i<=distance;i++){
-        currentCol = c+i;
-        if (currentCol > GRAPH_COLS || r > GRAPH_ROWS){
-            break;
+    for (int i =0; i<=distance;i++){ //Print distance+1 'ch's.
+        currentCol = c+i; //increments the column
+        if (currentCol > GRAPH_COLS || r > GRAPH_ROWS){ //exceeds the maximum column/row
+            break; //exits the loop
         }
         else{
-            setChar(r, currentCol, ch);
+            setChar(r, currentCol, ch); //prints character
         }
     }
 }
@@ -18,9 +18,9 @@ void plotVerticalLine(int r, int c, int distance, char ch){
     const int GRAPH_ROWS = getRows();
     const int GRAPH_COLS = getCols();
     int currentRow;
-    for (int i =0; i<=distance;i++){
-        currentRow = r+i;
-        if (currentRow > GRAPH_ROWS || c > GRAPH_COLS){
+    for (int i =0; i<=distance;i++){ //Print distance+1 'ch's.
+        currentRow = r+i; //increments the row
+        if (currentRow > GRAPH_ROWS || c > GRAPH_COLS){ //exceeds the maximum column/row
             break;
         }
         else{
@@ -31,8 +31,8 @@ void plotVerticalLine(int r, int c, int distance, char ch){
 void plotRectangle(int r, int c, int height, int width, char ch){
     plotHorizontalLine(r, c, width, ch);
     plotVerticalLine(r, c, height, ch);
-    plotHorizontalLine(r+width, c, width, ch);
-    plotVerticalLine(r, c+height, height, ch);
+    plotHorizontalLine(r+width, c, width, ch); //change the 'row' parameter
+    plotVerticalLine(r, c+height, height, ch); //change the 'column' parameter
 }
 int main()
 {
