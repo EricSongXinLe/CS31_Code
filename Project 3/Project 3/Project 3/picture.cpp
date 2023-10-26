a5620d0ece15ce849733a9ee9319c9c094622934
EricS <song_xinle@163.com>
2023-10-25 21:34:01 -0700

    PHASE 2 verification complete
    testing IP

--- Project 3/Project 3/Project 3/picture.cpp
+++ Project 3/Project 3/Project 3/picture.cpp
@@ -1,5 +1,15 @@
 #include "grid.h"
+#include <cctype>
+#include <iostream>
+#include <cassert>
+using namespace std;
 
+const int HORIZ = 0;
+const int VERT = 1;
+
+const int FG = 0;
+const int BG = 1;
+//-------------------START-----OF----PHASE----ONE----------------//
 void plotHorizontalLine(int r, int c, int distance, char ch){
     const int GRAPH_ROWS = getRows(); //check the maximum column/row in the graph.
     const int GRAPH_COLS = getCols();
@@ -31,12 +41,56 @@ void plotVerticalLine(int r, int c, int distance, char ch){
 void plotRectangle(int r, int c, int height, int width, char ch){
     plotHorizontalLine(r, c, width, ch);
     plotVerticalLine(r, c, height, ch);
-    plotHorizontalLine(r+width, c, width, ch); //change the 'row' parameter
-    plotVerticalLine(r, c+height, height, ch); //change the 'column' parameter
+    plotHorizontalLine(r+width, c, width, ch); //update the 'row' parameter to draw new line
+    plotVerticalLine(r, c+height, height, ch); //update the 'column' parameter to draw new line
 }
+
+//-------------------END-----OF----PHASE----ONE----------------//
+
+//-------------------START-----OF----PHASE----TWO----------------//
+bool validPlotLineParameter(int r, int c, int distance, int dir, char plotChar, int fgbg){
+    if( !(dir == HORIZ || dir == VERT)){ //DIRECTION is not horizontal or vertical
+        return false; //ends this sub-routine
+    }
+    if( !(fgbg == FG || fgbg == BG)){ //fgbg input is not valid
+        return false;
+    }
+    if(dir == HORIZ){ //only check COLUMN if direction is HORIZONTAL
+        if (c+distance <= 0 || c+distance > getCols()){
+            return false;
+        }
+    }
+    if(dir == VERT){  //only check ROWS if direction is VERTICAL
+        if (r+distance <=0 || r+distance > getRows()){
+            return false;
+        }
+    }
+    // r+distance and c+distance calculates the rightmost or leftmost char will be plotted. If the last character is in the plot, the whole line will be in the plot.
+    // return false; will be executed when ROW or COL exceeds the boundary of the plot. [ONLY 1 to getRows() or getCols() is accepted.]
+    
+    if( !isprint(plotChar)){
+        return false;
+    }
+    return true;
+}
+bool plotLine(int r, int c, int distance, int dir, char plotChar, int fgbg){
+    if( !validPlotLineParameter(r, c, distance, dir, plotChar, fgbg)){ //invalid parameter
+        return false; //ends funciton
+    }
+    return true;
+}
+
+
 int main()
 {
-    setSize(20, 30);
-    plotRectangle(2, 2, 5, 5, '%');
+    setSize(20, 15);  // grid has 20 rows, 15 columns
+    if ( ! plotLine(14, 8, 3, HORIZ, '*', FG))   // first call
+       cout << "1) Plotting failed when it shouldn't have!" << endl;
+    if ( ! plotLine(15, 10, -2, VERT, '@', FG))  // second call
+       cout << "2) Plotting failed when it shouldn't have!" << endl;
+    if ( ! plotLine(13, 8, 3, HORIZ, '#', BG))   // third call
+       cout << "3) Plotting failed when it shouldn't have!" << endl;
+    if (plotLine(13, 14, 3, HORIZ, 'X', FG))     // fourth call
+       cout << "4) Plotting succeeded when it shouldn't have!" << endl;
     draw();
 }
