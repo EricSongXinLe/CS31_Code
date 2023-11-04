3d1a3c0277a9d36002f8df943d89dd73e0d0b3b5
EricS <song_xinle@163.com>
2023-11-03 19:02:10 -0700

    finished reduplicate

--- Project 4/Project4/Project4/array.cpp
+++ Project 4/Project4/Project4/array.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-int locate(const string a[], int n, string target){
-    if(n<0){
+int reduplicate(string a[], int n){
+    if(n<0){ //invalid array length
         return -1;
     }
     for(int i=0; i<n;i++){
-        if (a[i] == target){
-            return i;
+        a[i].append(a[i]); //appends itself into element.
+    }
+    return n;
+}
+int locate(const string a[], int n, string target){
+    if(n<0){ //invalid array length
+        return -1;
+    }
+    for(int i=0; i<n;i++){
+        if (a[i] == target){ //found target
+            return i; //return index
         }
     }
     return -1;
 }
 int main(int argc, const char * argv[]) {
-    string people[5] = { "donald", "vivek", "tim", "asa", "nikki" };
-    int i = locate(people, 5, "nikki");
+    string stuff[6] = { "mahi", "bon", "cous", "", "tar", "mur" };
+    int i = reduplicate(stuff, 6);
+    for (int i =0; i<6;i++){
+        cerr<<stuff[i]<<endl;
+    }
     cerr <<i<<endl;
     return 0;
 }
