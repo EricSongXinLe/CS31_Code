3e741f48f7a89c7a3706a21ef379f41f79203398
EricS <song_xinle@163.com>
2023-11-15 20:18:15 -0800

    paragraph break

--- Project5/Project 5/Project 5/render.cpp
+++ Project5/Project 5/Project 5/render.cpp
@@ -62,27 +62,33 @@ bool getNextToken(istream& inf, char token[]){
 }
 
 void outputToken(bool& doubleSpace, int& counter, ostream& outf, char token[]){
+    
+    if(token[0] == '@' && token[1] == 'P' && token[2] == '@'){
+        outf<<'\n'<<'\n'; //Output a blank new line and start off a new line
+        counter = 0;
+        doubleSpace = false; //this prevents when running the outputToken the next time, additional spaces are produced.
+        return; //By returning, this breaks of the void outputToken function, to avoid to token '@''P''@' from being written to file.
+    }
+    if(doubleSpace == true){
+        outf<<' '; //ADDITIONAL SPACE.
+        counter++; //increment counter by 1 since one ADDITIONAL Space is produced.
+        doubleSpace = false;
+    }
     if(counter != 0){
-        if(doubleSpace == true){
-            outf<<' ';
-            counter++;
-            doubleSpace = false;
-        }
         if(token[0] != '\0'){
-            outf<<' ';
+            outf<<' '; //usual space after word.
             counter++;
         }
-        if(token[strlen(token)-1] == '.' || token[strlen(token)-1] == '?' || token[strlen(token)-1] == '!' || token[strlen(token)-1] == ':'){
-           doubleSpace = true;
-       }
-        
+    }
+    if(token[strlen(token)-1] == '.' || token[strlen(token)-1] == '?' || token[strlen(token)-1] == '!' || token[strlen(token)-1] == ':'){ //'For .?!: at the last char of a token, a additional space is needed.
+        doubleSpace = true; //set doubleSpace to be true, so one space will be printed before outputting the next token.
     }
     outf<<token;
     counter+=strlen(token);
 }
 
 void processToken(bool& doubleSpace, int& counter, int lineLength, ostream& outf, char token[]){
-    if(lineLength >= counter + strlen(token)+1){
+    if(lineLength >= counter + strlen(token)+1){ //This line hasn't finished yet, and can fit additional token & one space.
         outputToken(doubleSpace, counter, outf, token);
     }
     else{
