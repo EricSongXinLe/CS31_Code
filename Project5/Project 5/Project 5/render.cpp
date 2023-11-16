542ec9954256d5702b9cf9d29b7db7c96aa9bbbb
EricS <song_xinle@163.com>
2023-11-15 21:12:21 -0800

    fixed consecutive @P@ issue

--- Project5/Project 5/Project 5/render.cpp
+++ Project5/Project 5/Project 5/render.cpp
@@ -61,10 +61,18 @@ bool getNextToken(istream& inf, char token[]){
     return false; //can't get next char! reached EoF!
 }
 
-void outputToken(bool& doubleSpace, int& counter, ostream& outf, char token[]){
-    
-    if(token[0] == '@' && token[1] == 'P' && token[2] == '@'){
-        outf<<'\n'<<'\n'; //Output a blank new line and start off a new line
+void outputToken(int& countEmptyParagraph,bool& doubleSpace, int& counter, ostream& outf, char token[]){
+    if(token[0] == '@' && token[1] == 'P' && token[2] == '@' && strlen(token)==3){ //IF and ONLY IF token is 3 chars long, and only consists of @P@
+        if(countEmptyParagraph == 0){ //no empty paragraphs have been produced.
+            outf<<'\n'<<'\n'; //Output a blank new line and start off a new line
+            countEmptyParagraph++;
+        }
+        else{
+            counter = 0; //
+            countEmptyParagraph = 0;//reset empty paragraph, jump to next token.
+            doubleSpace = false; //this prevents when running the outputToken the next time, additional spaces are produced.
+            return; //By returning, this breaks of the void outputToken function, to avoid to token '@''P''@' from being written to file.
+        }
         counter = 0;
         doubleSpace = false; //this prevents when running the outputToken the next time, additional spaces are produced.
         return; //By returning, this breaks of the void outputToken function, to avoid to token '@''P''@' from being written to file.
@@ -74,31 +82,33 @@ void outputToken(bool& doubleSpace, int& counter, ostream& outf, char token[]){
         counter++; //increment counter by 1 since one ADDITIONAL Space is produced.
         doubleSpace = false;
     }
-    if(counter != 0){
+    if(counter != 0){ //Not first word
         if(token[0] != '\0'){
-            outf<<' '; //usual space after word.
+            outf<<' '; //usual space before word.
             counter++;
         }
     }
     if(token[strlen(token)-1] == '.' || token[strlen(token)-1] == '?' || token[strlen(token)-1] == '!' || token[strlen(token)-1] == ':'){ //'For .?!: at the last char of a token, a additional space is needed.
         doubleSpace = true; //set doubleSpace to be true, so one space will be printed before outputting the next token.
     }
+    //ABOVE IS ALL Pre-processing before outputting the token
     outf<<token;
     counter+=strlen(token);
 }
 
-void processToken(bool& doubleSpace, int& counter, int lineLength, ostream& outf, char token[]){
+void processToken(int& countEmptyParagraph, bool& doubleSpace, int& counter, int lineLength, ostream& outf, char token[]){
     if(lineLength >= counter + strlen(token)+1){ //This line hasn't finished yet, and can fit additional token & one space.
-        outputToken(doubleSpace, counter, outf, token);
+        outputToken(countEmptyParagraph, doubleSpace, counter, outf, token);
     }
     else{
         outf<<'\n';
         counter = 0;
-        outputToken(doubleSpace, counter, outf, token);
+        outputToken(countEmptyParagraph, doubleSpace, counter, outf, token);
     }
 }
 
 int render(int lineLength, istream& inf, ostream& outf){
+    int countEmptyParagraph = 0;
     bool doubleSpace = false;
     if(lineLength < 1){
         return 2;
@@ -107,9 +117,9 @@ int render(int lineLength, istream& inf, ostream& outf){
         int counter = 0;
         char token[MAX];
         while(getNextToken(inf,token)){
-            processToken(doubleSpace, counter, lineLength,outf,token);
+            processToken(countEmptyParagraph, doubleSpace, counter, lineLength,outf,token);
         }
-        processToken(doubleSpace, counter, lineLength,outf,token);
+        processToken(countEmptyParagraph, doubleSpace, counter, lineLength,outf,token);
         return 0;
     }
 }
