bbf1db60789b262d7d4dc6d664aac5b4aef2a1c2
EricS <song_xinle@163.com>
2023-11-18 17:24:14 -0800

    added helpful comments

--- Project5/Project 5/Project 5/render.cpp
+++ Project5/Project 5/Project 5/render.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cctype>
+#include <cstring>
 using namespace std;
 const int MAX = 180;
 /* ---------------------PHASE ONE START*/
@@ -54,10 +55,10 @@ bool getNextToken(int& tokenType, istream& inf, char token[]){
             return true;
         }
         else if(c == '-'){
-            token[tokenIndex] = c;
-            tokenType = 2;
+            token[tokenIndex] = c; //add the '-' to string
+            tokenType = 2; //Type 2 means the token includes '-', which means a space is not needed between the token and the next token
             tokenIndex++;
-            token[tokenIndex] = '\0';
+            token[tokenIndex] = '\0'; //end this token
             return true;
         }
         else{
@@ -91,7 +92,7 @@ void outputToken(int& prevTokenType, int& tokenType, int& countEmptyParagraph,bo
         doubleSpace = false; //this prevents when running the outputToken the next time, additional spaces are produced.
         return; //By returning, this breaks of the void outputToken function, to avoid to token '@''P''@' from being written to file.
     }
-    if(doubleSpace == true){
+    if(doubleSpace == true and counter != 0){ //Do not output ' ' if ' 'will be first character
         outf<<' '; //ADDITIONAL SPACE.
         counter++; //increment counter by 1 since one ADDITIONAL Space is produced.
         doubleSpace = false;
@@ -111,7 +112,7 @@ void outputToken(int& prevTokenType, int& tokenType, int& countEmptyParagraph,bo
     prevTokenType = tokenType;
 }
 
-void processToken(int& prevTokenType, int& tokenType, int& countEmptyParagraph, bool& doubleSpace, int& counter, int lineLength, ostream& outf, char token[]){
+void processToken(bool& returnOne, int& prevTokenType, int& tokenType, int& countEmptyParagraph, bool& doubleSpace, int& counter, int lineLength, ostream& outf, char token[]){
     int space;
     if(prevTokenType==1){ //if token is a regular word
         space = 1; //should include a space before next token
@@ -122,9 +123,30 @@ void processToken(int& prevTokenType, int& tokenType, int& countEmptyParagraph, 
     if(lineLength >= counter + strlen(token)+space){ //This line hasn't finished yet, and can fit additional token & optional space.
         outputToken(prevTokenType, tokenType, countEmptyParagraph, doubleSpace, counter, outf, token);
     }
+    else if (lineLength < strlen(token)){ //special case where word
+        if(! (prevTokenType==-1)){ // Type -1 means this is the first token every written, therefore do not need to output a new line.
+            outf<<'\n';
+        }
+        returnOne = true;
+        int charsToBeWritten = strlen(token); //how many characters will need to be written
+        int charsWritten = 0; //already written chars
+        while(charsToBeWritten > lineLength){ //While the chars to be written will fit more than one line
+            for(int i =charsWritten; i<charsWritten+lineLength;i++){ //write the chars to the whole  line
+                outf<<token[i];
+            }
+            charsToBeWritten-=lineLength; //Change charstobewritten and charsWritten accordingly, to prepare for the next loop
+            charsWritten+=lineLength;
+            outf<<'\n'; //Go to next line
+        }
+        for(int i = charsWritten; i<strlen(token);i++){ //The chars can be fitted into one line.
+            outf<<token[i];
+            counter++; //increment counter accordingly so other normal characters can be fitted into this line.
+        }
+        prevTokenType = 1; //Type 1 means a space is needed.
+    }
     else{
-        outf<<'\n';
-        counter = 0;
+        outf<<'\n'; //new line is needed.
+        counter = 0; //reset counter.
         outputToken(prevTokenType, tokenType, countEmptyParagraph, doubleSpace, counter, outf, token);
     }
 }
@@ -133,17 +155,21 @@ int render(int lineLength, istream& inf, ostream& outf){
     int countEmptyParagraph = 0;
     bool doubleSpace = false;
     if(lineLength < 1){
-        return 2;
+        return 2; //invalid input
     }
     else{
-        int counter = 0;
+        int counter = 0; //Counts how many chars was already printed
         char token[MAX];
         int tokenType;
-        int prevTokenType=0;
+        int prevTokenType=-1; //Default value is -1, because there is no previous token
+        bool returnOne = false;
         while(getNextToken(tokenType, inf,token)){
-            processToken(prevTokenType, tokenType, countEmptyParagraph, doubleSpace, counter, lineLength,outf,token);
+            processToken(returnOne, prevTokenType, tokenType, countEmptyParagraph, doubleSpace, counter, lineLength,outf,token);
         }
-        processToken(prevTokenType, tokenType, countEmptyParagraph, doubleSpace, counter, lineLength,outf,token);
+        processToken(returnOne, prevTokenType, tokenType, countEmptyParagraph, doubleSpace, counter, lineLength,outf,token);
+        //Line above handles the situation getNextToken returns false(reached last token of file) but there is still ONE MORE token that is to be processed.
+        if(returnOne == true) //flag is true
+            return 1;
         return 0;
     }
 }
@@ -151,5 +177,5 @@ int render(int lineLength, istream& inf, ostream& outf){
 int main() {
     ifstream infile("input.txt");
     ofstream outfile("result.txt");
-    return render(40,infile,outfile);
+    return render(4,infile,outfile);
 }
